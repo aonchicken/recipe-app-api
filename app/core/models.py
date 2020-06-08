ac7c6991a6e248f8dcc65591c33b7f1d7718add3
Ratakarn.Bunt <aonchicken@gmail.com>
2020-06-08 16:11:24 +0700

    edit flake 3

--- app/core/models.py
+++ app/core/models.py
@@ -6,8 +6,6 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 
 class UserManager(BaseUserManager):
-
-
     def create_user(self, email, password=None, **extra_fields):
         """Creates and save a new user"""
         if not email:
@@ -16,23 +14,20 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
+
     def create_superuser(self, email, password):
         """Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-
         return user
-    
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that suppors using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=True)
-
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
