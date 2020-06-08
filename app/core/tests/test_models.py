35d1da038b38f54a0ec518e6fd0c86a6d4cc736c
Ratakarn.Bunt <aonchicken@gmail.com>
2020-06-08 16:24:56 +0700

    edit flake8 5

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -25,7 +26,7 @@ class ModelTests(TestCase):
     def test_new_uesr_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None,'test123')
+            get_user_model().objects.create_user(None, 'test123')
 
     def test_create_new_superuser(self):
         """"Test creating a new superuser"""
